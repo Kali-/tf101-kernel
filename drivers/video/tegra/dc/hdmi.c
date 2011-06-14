51198ccdf6c7806272198e7eb068ec678eb56113
Donghan Ryu <dryu@nvidia.com>
2011-06-14 15:18:22 +0900

    video: tegra: remove 1080p@30HZ define from kernel
    CONFIG_TEGRA_ENABLE_SUPPORT_FOR_1080p_30HZ define is not needed
    anymore since this can be filtered from user space driver
    
    Bug: 815409
    
    Change-Id: I3fd37d2c3e737aaa056aea2af7b41ac5b93b9807
    Reviewed-on: http://git-master/r/36435
    Reviewed-by: Michael I Gold <gold@nvidia.com>
    Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>
    Reviewed-by: Donghan Ryu <dryu@nvidia.com>
    Tested-by: Donghan Ryu <dryu@nvidia.com>

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -146,8 +146,6 @@ const struct fb_videomode tegra_dc_hdmi_supported_modes[] = {
 		.vmode =	FB_VMODE_NONINTERLACED,
 		.sync = 0,
 	},
-
-#ifdef CONFIG_TEGRA_ENABLE_SUPPORT_FOR_1080p_30HZ
 	/* 1920x1080p 30Hz EIA/CEA-861-B Format 34 */
 	{
 		.xres =		1920,
@@ -162,7 +160,6 @@ const struct fb_videomode tegra_dc_hdmi_supported_modes[] = {
 		.vmode =	FB_VMODE_NONINTERLACED,
 		.sync = FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
 	},
-#else
 	/* 1920x1080p 59.94/60hz EIA/CEA-861-B Format 16 */
 	{
 		.xres =		1920,
@@ -177,7 +174,6 @@ const struct fb_videomode tegra_dc_hdmi_supported_modes[] = {
 		.vmode =	FB_VMODE_NONINTERLACED,
 		.sync = FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
 	},
-#endif
 };
 
 /* table of electrical settings, must be in acending order. */
